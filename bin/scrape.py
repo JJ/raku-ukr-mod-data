38cc6629f1a931db176ada9ff263520131071361
JJ Merelo <jjmerelo@gmail.com>
2022-06-09 20:56:32 +0200

    This seems to work

--- bin/scrape.py
+++ bin/scrape.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python
 
-from selenium import webdriver
+import undetected_chromedriver as uc
 import io
 
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-# Change chromedriver path accordingly
-driver = webdriver.Chrome("/opt/google/chrome/chromedriver", options=options)
+driver = uc.Chrome(driver_executable_path='/opt/google/chrome/chromedriver')
 driver.get("https://www.mil.gov.ua/en/news/2022/05/25/the-total-combat-losses-of-the-enemy-from-24-02-to-25-05/")
-driver.implicitly_wait(30)
-html = driver.page_source
-with io.open(driver.title + ".html", "w", encoding="utf-8") as f:
-    f.write(html)
-    f.close()
-driver.quit()
