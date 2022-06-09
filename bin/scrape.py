a372742bdc68b6ffb51e8d434eda59e082dad873
JJ Merelo <jjmerelo@gmail.com>
2022-06-09 21:02:23 +0200

    Got it working

--- bin/scrape.py
+++ bin/scrape.py
@@ -3,5 +3,8 @@
 import undetected_chromedriver as uc
 import io
 
-driver = uc.Chrome(driver_executable_path='/opt/google/chrome/chromedriver')
+driver = uc.Chrome(
+    driver_executable_path='/opt/google/chrome/chromedriver', headless=True
+)
 driver.get("https://www.mil.gov.ua/en/news/2022/05/25/the-total-combat-losses-of-the-enemy-from-24-02-to-25-05/")
+print(driver.page_source)
