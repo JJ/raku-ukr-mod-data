c84d6e9d541c4afae0bb111453b2e4a65309e031
JJ Merelo <jjmerelo@gmail.com>
2022-06-10 17:32:55 +0200

    Changes to use from time to time

--- bin/scrape.py
+++ bin/scrape.py
@@ -3,6 +3,20 @@
 import undetected_chromedriver as uc
 import time
 import random
+import sys
+
+
+def download(driver, day, month):
+            url = f'https://www.mil.gov.ua/en/news/2022/{month}/{day}/the-total-combat-losses-of-the-enemy-from-24-02-to-{day}-{month}/'
+        print("⬇️ Download " + url)
+        driver.get(url)
+        filename = f'raw-pages/The total combat losses of the enemy from 24.02 to {day}.{month} | Міноборони.html'
+        print("💾 saving " + filename)
+        with open(filename, "w") as file:
+            file.write(driver.page_source)
+        sleeping = random.randrange(5, 15)
+        print(f'⌛ waiting for {sleeping}')
+        time.sleep(sleeping)
 
 driver = uc.Chrome(
     driver_executable_path='/opt/google/chrome/chromedriver', headless=True
@@ -14,15 +28,13 @@ days = {
     "02": [i for i in range(28, 25, -1)],
 }
 
-for month in days:
-    for day in days[month]:
-        url = f'https://www.mil.gov.ua/en/news/2022/{month}/{day}/the-total-combat-losses-of-the-enemy-from-24-02-to-{day}-{month}/'
-        print("⬇️ Download " + url)
-        driver.get(url)
-        filename = f'raw-pages/The total combat losses of the enemy from 24.02 to {day}.{month} | Міноборони.html'
-        print("💾 saving " + filename)
-        with open(filename, "w") as file:
-            file.write(driver.page_source)
-        sleeping = random.randrange(5, 15)
-        print(f'⌛ waiting for {sleeping}')
-        time.sleep(sleeping)
+def main() -> int:
+    if isset(sys.argv[1]):
+        download(driver,sys.argv[1],sys.argv[2])
+    else:
+        for month in days:
+            for day in days[month]:
+                download(driver,day, month)
+
+if __name__ == '__main__':
+    sys.exit(main())
