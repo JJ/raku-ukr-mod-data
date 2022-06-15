fa0ac0f15a6ec1ce094e33f72b2f971553cc2b7d
JJ Merelo <jjmerelo@gmail.com>
2022-06-15 20:37:54 +0200

    Updated expression of dates

--- ukr_mod_data/__init__.py
+++ ukr_mod_data/__init__.py
@@ -21,10 +21,10 @@ driver = uc.Chrome(
     driver_executable_path='/opt/google/chrome/chromedriver', headless=True
 )
 days = {
-    "05": [i for i in range(26, 1, -1)],
-    "04": [i for i in range(30, 1, -1)],
-    "03": [i for i in range(31, 1, -1)],
-    "02": [i for i in range(28, 25, -1)],
+    "06": [i for i in range(9, 1, -1)],
+    "05": [i for i in range(9, 1, -1)],
+    "04": [i for i in range(9, 1, -1)],
+    "03": [i for i in range(9, 1, -1)],
 }
 
 
