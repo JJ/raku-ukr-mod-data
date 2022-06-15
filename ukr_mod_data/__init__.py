82f68dc54e568c89e1e05ce8dc50fd4f9f7b87a3
JJ Merelo <jjmerelo@gmail.com>
2022-06-15 20:31:01 +0200

    Moving to a module

--- ukr_mod_data/__init__.py
+++ ukr_mod_data/__init__.py
@@ -5,7 +5,7 @@ import sys
 
 
 def download(driver, day, month):
-    url = f'https://www.mil.gov.ua/en/news/2022/{month}/{day}/the-total-combat-losses-of-the-enemy-from-24-02-to-{day}-{month}/'
+    url = f'https://www.mil.gov.ua/en/news/2022/{month}/{day}/the-total-combat-losses-of-the-enemy-from-24-02-to-{day:02}-{month}/'
     print("⬇️ Download " + url)
     driver.get(url)
     filename = f'raw-pages/The total combat losses of the enemy from 24.02 to {day}.{month} | Міноборони.html'
@@ -15,6 +15,7 @@ def download(driver, day, month):
     sleeping = random.randrange(5, 15)
     print(f'⌛ waiting for {sleeping}')
     time.sleep(sleeping)
+
 
 driver = uc.Chrome(
     driver_executable_path='/opt/google/chrome/chromedriver', headless=True
@@ -26,13 +27,15 @@ days = {
     "02": [i for i in range(28, 25, -1)],
 }
 
+
 def main() -> int:
     if len(sys.argv) > 1:
-        download(driver,sys.argv[1],sys.argv[2])
+        download(driver, sys.argv[1], sys.argv[2])
     else:
         for month in days:
             for day in days[month]:
-                download(driver,day, month)
+                download(driver, day, month)
+
 
 if __name__ == '__main__':
     sys.exit(main())
