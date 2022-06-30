130b0b38cd9ebcae0ef9e9508ba03689f64e7274
JJ Merelo <jjmerelo@gmail.com>
2022-06-30 17:46:45 +0200

    Test-disabling

--- ukr_mod_data/__init__.py
+++ ukr_mod_data/__init__.py
@@ -5,6 +5,7 @@ import sys
 
 
 def download(driver, day, month):
+    # pylint: disable=line-too-long
     url = f'https://www.mil.gov.ua/en/news/2022/{month}/{day}/the-total-combat-losses-of-the-enemy-from-24-02-to-{day:02}-{month}/'
     print("⬇️ Download " + url)
     driver.get(url)
