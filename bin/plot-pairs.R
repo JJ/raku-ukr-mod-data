f8045e73cdae3ac0c5a1208802913cb9b839570b
JJ Merelo <jjmerelo@gmail.com>
2022-07-05 17:16:05 +0200

    Added rscript for processing the CSV file and make certain plots

--- bin/plot-pairs.R
+++ bin/plot-pairs.R
@@ -1,2 +1,7 @@
+library(reshape2)
 
-data = read.csv("../resources/ukr-mod-data.csv")
+data = read.csv("resources/ukr-mod-data.csv")
+data.totals <- subset( data, select=-c(Delta))
+data.totals$Date <- as.Date( paste0("2022.",data.totals$Date), format="%Y.%d.%m")
+data.shaped <- dcast(data.totals, Date ~ Item )
+pairs(data.shaped)
