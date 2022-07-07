b4902ff4610eb69661f550fe1a7f42f9c0cdb988
JJ Merelo <jjmerelo@gmail.com>
2022-07-07 17:25:06 +0200

    Working with #34
    Generating automatically charts too

--- bin/plot-pairs.R
+++ bin/plot-pairs.R
@@ -5,7 +5,11 @@ data = read.csv("resources/ukr-mod-data.csv")
 data.totals <- subset( data, select=-c(Delta))
 data.totals$Date <- as.Date( paste0("2022.",data.totals$Date), format="%Y.%d.%m")
 totals.shaped <- dcast(data.totals, Date ~ Item )
-data.delta <- subset( data, select=-c(data$Totals))
+
+data.delta <- subset( data, select=-c(Total))
 data.delta$Date <- as.Date( paste0("2022.",data.delta$Date), format="%Y.%d.%m")
 delta.shaped <- dcast(data.delta, Date ~ Item )
+png("assets/delta-correlation-plot.png", height = 8, width = 8)
+ggpairs(delta.shaped,columns=2:12)
+dev.off()
 
