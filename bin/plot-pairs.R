0e93d337c2d615f317c31dbe7ef131e0403cc8fc
JJ Merelo <jjmerelo@gmail.com>
2022-07-07 17:18:13 +0200

    Trying to reshape and work with columns

--- bin/plot-pairs.R
+++ bin/plot-pairs.R
@@ -1,7 +1,11 @@
 library(reshape2)
+library(GGally)
 
 data = read.csv("resources/ukr-mod-data.csv")
 data.totals <- subset( data, select=-c(Delta))
 data.totals$Date <- as.Date( paste0("2022.",data.totals$Date), format="%Y.%d.%m")
-data.shaped <- dcast(data.totals, Date ~ Item )
-pairs(data.shaped)
+totals.shaped <- dcast(data.totals, Date ~ Item )
+data.delta <- subset( data, select=-c(data$Totals))
+data.delta$Date <- as.Date( paste0("2022.",data.delta$Date), format="%Y.%d.%m")
+delta.shaped <- dcast(data.delta, Date ~ Item )
+
